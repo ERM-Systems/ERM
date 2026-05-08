0314e20847ca6a9d7fb6a827c76a7067d3a5d673
ar-cyber <loafing@sockycat.net>
2026-05-09 08:13:35 +0930

    looks like that aint working

--- tasks/check_ping.py
+++ tasks/check_ping.py
@@ -4,8 +4,10 @@ from erm import Bot
 import time
 @tasks.loop(seconds=5)
 async def check_ping(bot: Bot):
-    bot.saved_latencies["shards"].append(round(sum(l for _, l in bot.latencies) / len(bot.latencies) * 1000))
-
+    try:
+        bot.saved_latencies["shards"].append(round(sum(l for _, l in bot.latencies) / len(bot.latencies) * 1000))
+    except:
+        pass
     before = time.monotonic()
     user = await bot.fetch_user(993781395761676298)
     after = time.monotonic()
@@ -18,7 +20,3 @@ async def check_ping(bot: Bot):
     db_latency = round((after - before) * 1000)
     bot.saved_latencies["db"].append(db_latency)
 
-@check_ping.error
-async def check_ping_e(error):
-    print(f"Latency task error: {error}")
-    check_ping.restart()
