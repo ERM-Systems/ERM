b4085a2c72f8569cf52b9c57181aa28251a99679
ar-cyber <loafing@sockycat.net>
2026-05-09 08:06:16 +0930

    stop the task from crashing

--- tasks/check_ping.py
+++ tasks/check_ping.py
@@ -5,6 +5,7 @@ import time
 @tasks.loop(seconds=5)
 async def check_ping(bot: Bot):
     bot.saved_latencies["shards"].append(round(sum(l for _, l in bot.latencies) / len(bot.latencies) * 1000))
+
     before = time.monotonic()
     user = await bot.fetch_user(993781395761676298)
     after = time.monotonic()
@@ -16,3 +17,8 @@ async def check_ping(bot: Bot):
     after = time.monotonic()
     db_latency = round((after - before) * 1000)
     bot.saved_latencies["db"].append(db_latency)
+
+@check_ping.error
+async def check_ping_e(error):
+    print(f"Latency task error: {error}")
+    check_ping.restart()
